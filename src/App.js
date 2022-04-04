8db0594f386d1d8f9d8e5b8cf17a4e15bcd80122
Marko Livaic <makifrane@gmail.com>
2022-04-04 22:47:40 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import useStyles from "./styles";
+
+import Details from "./Components/Details/Details";
+import Main from "./Components/Main/Main";
 
 const App = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+        className={classes.grid}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  Typography,
+  Grid,
+  Divider,
+  CardContent,
+} from "@material-ui/core";
+import { mergeClasses } from "@material-ui/styles";
+
+import useStyles from "./styles";
+import Form from "./Form/Form";
+
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          Try saying: Add income for $100 in Catergory Salary for Monday
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* <List /> */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
