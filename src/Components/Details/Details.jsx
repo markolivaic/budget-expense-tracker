d3cf4889873e4675f5c4823c8c5794b1f6b36357
Marko Livaic <makifrane@gmail.com>
2022-04-06 18:43:27 +0200

    push

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Line } from "react-chartjs-2";
 
 import useStyles from "./styles";
+import useTransaction from "../../useTransactions";
 
 const Details = ({ title }) => {
   const classes = useStyles();
+  const { total, chartData } = useTransaction(title);
   return (
     <Card className={title === "Income" ? classes.income : classes.expense}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">$50</Typography>
-        {/* <Doughnut data="DATA" /> */}
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   );
